762f8574247c67b4f2063463a0e463cd475d372b
vybukhivka <viktorbakhovka@gmail.com>
2026-04-03 12:29:55 +0300

    proper delay, based on cpu ticks

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,8 +1,17 @@
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/l4/rcc.h>
 
 #define LED_PORT (GPIOA)
 #define LED_PIN (GPIO5)
+#define CPU_FREQ (80000000)
+#define SYSTICK_FREQ (1000)
+
+volatile uint64_t ticks = 0;
+void sys_tick_handler(void) { ticks++; }
+
+static uint64_t get_ticks(void) { return ticks; }
 
 static void rcc_setup(void) {
   rcc_periph_clock_enable(RCC_GPIOA);
@@ -13,19 +22,24 @@ static void gpio_setup(void) {
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
-static void delay_cycles(uint32_t cycles) {
-  for (uint32_t i = 0; i < cycles; i++) {
-    __asm__("nop");
-  }
+static void systick_setup(void) {
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+  systick_counter_enable();
+  systick_interrupt_enable();
 }
 
 int main(void) {
   rcc_setup();
   gpio_setup();
+  systick_setup();
+
+  uint64_t start_time = get_ticks();
 
   while (1) {
-    gpio_toggle(LED_PORT, LED_PIN);
-    delay_cycles(80000000 / 4);
+    if (get_ticks() - start_time >= 100) {
+      gpio_toggle(LED_PORT, LED_PIN);
+      start_time = get_ticks();
+    }
   }
 
   return 0;
