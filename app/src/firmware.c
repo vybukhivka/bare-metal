3a8dfa97dd68c4bd60ee9a623efd93eeb469f04c
vybukhivka <viktorbakhovka@gmail.com>
2026-04-03 22:59:19 +0300

    clang formater setup

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,46 +1,44 @@
 #include "core/system.h"
 #include "core/timer.h"
+#include <libopencm3/cm3/scb.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/l4/rcc.h>
-#include <libopencm3/cm3/scb.h>
 
 #define BOOTLOADER_SIZE (0x8000U)
 
-#define LED_PORT        (GPIOA)
-#define LED_PIN         (GPIO5)
+#define LED_PORT (GPIOA)
+#define LED_PIN  (GPIO5)
 
-static void vector_setup(void) {
-	SCB_VTOR = BOOTLOADER_SIZE;
-}
+static void vector_setup(void) { SCB_VTOR = BOOTLOADER_SIZE; }
 
 static void gpio_setup(void) {
-	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
-	gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
+    gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
 }
 
 int main(void) {
-	vector_setup();
-	system_setup();
-	gpio_setup();
-	timer_setup();
+    vector_setup();
+    system_setup();
+    gpio_setup();
+    timer_setup();
 
-	uint64_t start_time = system_get_ticks();
-	float duty_cycle = 0.0f;
+    uint64_t start_time = system_get_ticks();
+    float duty_cycle    = 0.0f;
 
-	timer_pwm_set_duty_cycle(duty_cycle);
+    timer_pwm_set_duty_cycle(duty_cycle);
 
-	while (1) {
-		if (system_get_ticks() - start_time >= 10) {
-			duty_cycle += 1.0f;
-			if (duty_cycle > 100.0f) {
-				duty_cycle = 0.0f;
-			}
-			timer_pwm_set_duty_cycle(duty_cycle);
+    while (1) {
+        if (system_get_ticks() - start_time >= 10) {
+            duty_cycle += 1.0f;
+            if (duty_cycle > 100.0f) {
+                duty_cycle = 0.0f;
+            }
+            timer_pwm_set_duty_cycle(duty_cycle);
 
-			start_time = system_get_ticks();
-		}
-	}
+            start_time = system_get_ticks();
+        }
+    }
 
-	return 0;
+    return 0;
 }
