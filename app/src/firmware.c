148f0e12a39ac52ceea34f2339dc8cc5bb1c82a2
vybukhivka <viktorbakhovka@gmail.com>
2026-04-03 14:55:32 +0300

    wip: not compiling with timer

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,4 +1,5 @@
 #include "core/system.h"
+#include "core/timer.h"
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/l4/rcc.h>
 
@@ -6,21 +7,32 @@
 #define LED_PIN (GPIO5)
 
 static void gpio_setup(void) {
-  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
+	gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
 }
 
 int main(void) {
-  system_setup();
-  gpio_setup();
+	system_setup();
+	gpio_setup();
+	timer_setup();
 
-  uint64_t start_time = system_get_ticks();
+	uint64_t start_time = system_get_ticks();
+	float duty_cycle = 0.0f;
 
-  while (1) {
-    if (system_get_ticks() - start_time >= 1000) {
-      gpio_toggle(LED_PORT, LED_PIN);
-      start_time = system_get_ticks();
-    }
-  }
+	timer_pwm_set_duty_cycle(duty_cycle);
 
-  return 0;
+	while (1) {
+		if (system_get_ticks() - start_time >= 1000) {
+			duty_cycle += 1.0f;
+			if (duty_cycle > 100.0f) {
+				duty_cycle = 0.0f;
+			}
+			timer_pwm_set_duty_cycle(duty_cycle);
+
+			start_time = system_get_ticks();
+		}
+	}
+
+	return 0;
 }
