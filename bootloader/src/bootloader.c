1b678e130d0e9df775b7ab1e5d7cb8cd396b7277
vybukhivka <viktorbakhovka@gmail.com>
2026-04-04 16:39:09 +0300

    refactor: vector_table_t struct used for navigation to main

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,3 +1,4 @@
+#include <libopencm3/cm3/vector.h>
 #include <libopencm3/stm32/memorymap.h>
 #include <stdint.h>
 
@@ -5,18 +6,14 @@
 #define MAIN_APP_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE)
 
 static void jump_to_main(void) {
-	typedef void (*void_fn)(void);
-
-	uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);
-	uint32_t* reset_vector = (uint32_t*)*reset_vector_entry;
-	void_fn jump_fn = (void_fn)reset_vector;
-
-	jump_fn();
+    vector_table_t *main_vector_table =
+        (vector_table_t *)(MAIN_APP_START_ADDRESS);
+    main_vector_table->reset();
 }
 
 int main(void) {
-	jump_to_main();
+    jump_to_main();
 
-	// Never return
-	return 0;
+    // Never return
+    return 0;
 }
