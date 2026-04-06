ed01892825219be1110109b6d1f6d7302b84788e
vybukhivka <viktorbakhovka@gmail.com>
2026-04-06 14:52:04 +0300

    fix: lsp include issues

--- shared/src/core/system.c
+++ shared/src/core/system.c
@@ -8,19 +8,19 @@ static volatile uint64_t ticks = 0;
 void sys_tick_handler(void) { ticks++; }
 
 static void rcc_setup(void) {
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_clock_setup_pll(&rcc_hsi16_configs[RCC_CLOCK_VRANGE1_80MHZ]);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_clock_setup_pll(&rcc_hsi16_configs[RCC_CLOCK_VRANGE1_80MHZ]);
 }
 
 static void systick_setup(void) {
-	systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
-	systick_counter_enable();
-	systick_interrupt_enable();
+    systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
 }
 
 uint64_t system_get_ticks(void) { return ticks; }
 
 void system_setup(void) {
-	rcc_setup();
-	systick_setup();
+    rcc_setup();
+    systick_setup();
 }
