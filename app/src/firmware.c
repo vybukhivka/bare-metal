dcff8adead3b49bcd873fc60b01e1c43359f2cd5
vybukhivka <viktorbakhovka@gmail.com>
2026-04-06 17:11:47 +0300

    wip: UART working, but sends letters in infinite loop

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,13 +1,20 @@
-#include "core/system.h"
-#include "timer.h"
 #include <libopencm3/cm3/scb.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/l4/rcc.h>
+#include <stdint.h>
+
+#include "core/system.h"
+#include "core/uart.h"
+#include "timer.h"
 
 #define BOOTLOADER_SIZE (0x8000U)
 
 #define LED_PORT (GPIOA)
 #define LED_PIN  (GPIO5)
+
+#define UART_PORT (GPIOA)
+#define RX_PIN    (GPIO3)
+#define TX_PIN    (GPIO2)
 
 static void vector_setup(void) { SCB_VTOR = BOOTLOADER_SIZE; }
 
@@ -15,6 +22,9 @@ static void gpio_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOA);
     gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
     gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
+
+    gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, TX_PIN | RX_PIN);
+    gpio_set_af(UART_PORT, GPIO_AF7, TX_PIN | RX_PIN);
 }
 
 int main(void) {
@@ -22,9 +32,10 @@ int main(void) {
     system_setup();
     gpio_setup();
     timer_setup();
+    uart_setup();
 
     uint64_t start_time = system_get_ticks();
-    float duty_cycle    = 0.0f;
+    float duty_cycle = 0.0f;
 
     timer_pwm_set_duty_cycle(duty_cycle);
 
@@ -38,6 +49,13 @@ int main(void) {
 
             start_time = system_get_ticks();
         }
+
+        if (uart_data_available()) {
+            uint8_t data = uart_read_byte();
+            uart_write_byte(data + 1);
+        }
+
+        // Code
     }
 
     return 0;
